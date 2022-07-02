46a4c350d059c1d054c818128c99887399c51d46
sapede <matheussapede@gmail.com>
2022-07-02 15:37:38 -0400

    Adiciona Navbar

--- ThruPizza-front/front/src/components/Navbar.vue
+++ ThruPizza-front/front/src/components/Navbar.vue
@@ -1,13 +1,38 @@
 <template>
-    <div>
-
-    </div>
+    <v-app-bar app color="white" flat>
+        <v-badge color="#D5F0DB" dot>
+            <v-img src="../../public/1.png" contain width="60"></v-img>
+        </v-badge>
+        <v-toolbar-title class="ml-4">
+            <span class="green--text">e</span><strong>VrumVrumPizza</strong>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-badge color="#FF6D59" overlap content="2" class="mr-2 mt-1">
+            <v-avatar color="#FFF0EE" size="40">
+                <v-icon x-small color="#FF6D59">far fa-heart</v-icon>
+            </v-avatar>
+        </v-badge>
+        <v-badge color="#41AB55" overlap content="3" class="mr-2 mt-1">
+            <v-avatar color="#ECF7EE" size="40">
+                <v-icon x-small color="#41AB55">fas fa-shopping-cart</v-icon>
+            </v-avatar>
+        </v-badge>
+        <span class="grey--text d-nome d-sm-flex">Hello, <strong>Matuti</strong></span>
+        <v-avatar size="30" class="ml-2">
+            <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+        </v-avatar>
+        <v-btn icon dark>
+            <v-icon x-small color="#878A94">fas fa-chevron-down</v-icon>
+        </v-btn>
+    </v-app-bar>
 </template>
 <script>
 export default {
 
 }
 </script>
-<style lang="">
-    
+<style >
+.v-toolbar__title{
+    font-size: 1rem !important;
+}
 </style>
--- ThruPizza-front/front/src/views/HomeView.vue
+++ ThruPizza-front/front/src/views/HomeView.vue
@@ -1,17 +1,75 @@
 <template>
   <v-app>
-    
+    <Navbar />
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12">
+          <v-card class="mx-auto rounded-xl" flat>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/cooking.jpg" max-height="100%" max-width="100%" > </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import Navbar from '@/components/Navbar.vue'
 
-  export default {
-    name: 'Home',
+export default {
+  name: 'Home',
 
-    components: {
-      HelloWorld,
-    },
-  }
+  components: {
+    Navbar
+  },
+}
 </script>
+<style>
+.marginLeft {
+  margin-left: -90px;
+}
+
+.mtop {
+  margin-top: 100px;
+}
+
+.mbottom {
+  margin-bottom: 100px;
+}
+
+.v-card.borderme {
+  border: 2px solid green !important;
+}
+
+.v-card.borderout {
+  border: 1px solid #d5f0db !important;
+}
+
+.v-btn:not(.v-btn--round).v-size--default.add {
+  min-width: 0px !important;
+}
+
+.theme--light.v-sheet--outlined.mobile {
+  border: 2px solid black !important;
+}
+
+@media only screen and (max-width: 600px) {
+  h2.title1 {
+    font-size: 15px;
+  }
+
+  h2.title2 {
+    font-size: 15px;
+  }
+
+  .top {
+    margin-top: 20px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    .top {
+      margin-top: 120px;
+    }
+  }
+}
+</style>
